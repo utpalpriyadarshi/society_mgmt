591bbdfe9a4f397f9ad1608b869a321b8e8f83fd
[Utpal Priyadarshi] <[utpalpryadarshi@gmai.com]>
2025-09-19 19:58:27 +0530

    Remove /exe folder from tracking and add to .gitignore

--- exe/debug_run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Running Society Management System with console output for debugging...
-
-REM Run the executable with console enabled to see error messages
-dist\SocietyManagementSystem.exe
-
-echo.
-echo Application exited with error code: %errorlevel%
-pause
